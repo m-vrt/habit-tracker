09b640ca7f26f51b013b99f47c882e9c8adc4d55
MV <160310122+m-vrt@users.noreply.github.com>
2024-03-29 01:00:43 +0800

    Rename files/Add new files

---
+++ habit_tracker.py
@@ -0,0 +1,5 @@
+class Habit:
+    def __init__(self, name, periodicity, task_description):
+        self.name = name
+        self.periodicity = periodicity
+        self.task_description = task_description
