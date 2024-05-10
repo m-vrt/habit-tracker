b577974178f583b2d904780a91707a8ad93da608
MV <160310122+m-vrt@users.noreply.github.com>
2024-05-10 20:27:36 +0800

    Revise test_analytics.py

--- test_analytics.py
+++ test_analytics.py
@@ -1,9 +1,9 @@
 from analytics import (
     get_habit_hall_of_fame_daily,
-    get_habit_hall_of_fame_weekly,
-    get_habits_with_longest_streaks
+    get_habit_hall_of_fame_weekly,    
+    calculate_longest_streak_for_habit_predefined_daily,
+    calculate_longest_streak_for_habit_predefined_weekly
 )
-
 
 def test_get_habit_hall_of_fame_daily_with_data():
     streak = get_habit_hall_of_fame_daily()
@@ -13,10 +13,12 @@ def test_get_habit_hall_of_fame_weekly_with_data():
     streak = get_habit_hall_of_fame_weekly()
     assert streak > 0
 
-def test_get_habits_with_longest_streaks_daily():
-    habits = get_habits_with_longest_streaks("Daily")
-    assert len(habits) > 0
+def test_calculate_longest_streak_for_habit_predefined_daily():
+    assert calculate_longest_streak_for_habit_predefined_daily("Study") == 31
+    assert calculate_longest_streak_for_habit_predefined_daily("Code") == 6
+    assert calculate_longest_streak_for_habit_predefined_daily("Research") == 5
+ 
+def test_calculate_longest_streak_for_habit_predefined_weekly():
+    assert calculate_longest_streak_for_habit_predefined_weekly("Clean House") == 3
+    assert calculate_longest_streak_for_habit_predefined_weekly("Call Family") == 4
 
-def test_get_habits_with_longest_streaks_weekly():
-    habits = get_habits_with_longest_streaks("Weekly")
-    assert len(habits) > 0
